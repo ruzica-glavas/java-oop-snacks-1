5b80c4c0cbdf5ca96282717392892839dcff3081
ruzica-glavas <ruzi99@hotmail.it>
2025-05-05 19:36:10 +0200

    add: registro studenti

--- src/org/snacks/java/oop/Main.java
+++ src/org/snacks/java/oop/Main.java
@@ -9,6 +9,12 @@ public class Main {
         Studente s = new Studente("Simone", "Rossi", 15);
         System.out.println("Il nome, cognome e l'età dello studente sono:" + s.getStudent() + " " + "anni");
 
+        Studente m = new Studente("Marco", "Galli", 17);
+        System.out.println("Il nome, cognome e l'età dello studente sono:" + m.getStudent() + " " + "anni");
+
+        Studente g = new Studente("Giovanna", "Bianchi", 16);
+        System.out.println("Il nome, cognome e l'età dello studente sono:" + g.getStudent() + " " + "anni");
+
         //Snack conto bancario (2)
         ContoBancario cb= new ContoBancario("123456789012", new BigDecimal(500.50));
         System.out.println("Il suo numero di conto é il seguente:" + cb.getConto());
@@ -17,5 +23,18 @@ public class Main {
 
         cb.deposito(new BigDecimal(300));
         System.out.println("Saldo rimanente dopo il deposito:" + cb.getSaldo());
+
+        //Snack registro studenti (2)
+        RegistroStudenti registroS = new RegistroStudenti();
+
+        System.out.println(registroS);
+        registroS.addStudente(s);
+        
+        System.out.println(registroS.toString());
+
+        registroS.addStudente(m);
+        registroS.addStudente(g);
+
+        System.out.println(registroS.toString());
     }
 }
--- src/org/snacks/java/oop/RegistroStudenti.java
+++ src/org/snacks/java/oop/RegistroStudenti.java
@@ -1,5 +1,40 @@
 package org.snacks.java.oop;
 
 public class RegistroStudenti {
-    
+    private Studente [] registro;
+
+    public RegistroStudenti(){
+        this.registro = new Studente[0]; //Costruttore
+    }
+
+    public Studente [] getRegistro(){
+        return this.registro;
+    }
+
+    public void setRegistro(Studente[]registro){
+        this.registro=registro;
+    }
+
+    //Aggiungere studente all'array
+
+    public void addStudente(Studente studente){
+        Studente[] registroAggiornato = new Studente[this.registro.length+1]; // creazione di un nuovo registro lungo come quello creato precedentemente aggiungendo un elemento
+
+        for (int i=0; i<this.registro.length; i++){
+            registroAggiornato[i]=this.registro[i];
+        } //copia del mio vecchio registro nel nuovo, ciclando studente per studente
+
+        registroAggiornato[registroAggiornato.length-1]=studente; // aggiunta alla fine dell'array l'ulitmo studente
+
+        this.registro=registroAggiornato; //sovrascrittura del mio nuovo registro a quello vecchio
+    }
+
+    public String toString(){
+        String output = "Registro studenti:";
+        for (Studente studente: this.registro){
+            output += String.format("%s;", studente.getStudent());
+        }
+        return output;
+    }
+
 }
